35700994848392f77fb1e8b99d49e0beec42b2b0
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2024-02-14 15:05:00 +0100

    Configure extensions via pkg-config.

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup, find_packages, Extension
 
 import bolero
+import pkgconfig
+
+conf = pkgconfig.parse("bl_loader bolero lib_manager")
+
+ranking = Extension('bolero/utils/_ranking_svm', ['bolero/utils/_ranking_svm.cpp'])
+wrapper = Extension('bolero/wrapper/_wrapper', ['bolero/wrapper/_wrapper.cpp'],
+	include_dirs = conf["include_dirs"],
+	library_dirs = conf["library_dirs"],
+	libraries = conf["libraries"]
+)
 
 setup(
 	name="bolero",
@@ -12,8 +22,7 @@ setup(
 	url="http://robotik.dfki-bremen.de/en/research/softwaretools/bolero.html",
 	download_url="https://github.com/rock-learning/bolero/archive/master.zip",
 	scripts = ["bolero/scripts/bolero-run"],
-
 	packages=find_packages(),
-	ext_modules=[Extension('bolero/utils/_ranking_svm', ['bolero/utils/_ranking_svm.c'])],
+	ext_modules=[ranking, wrapper],
 )
 

1f3fa0fb6c6df833c3d7be5800442a45c382d668
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2024-02-16 08:33:56 +0100

    call cython from setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,17 +1,25 @@
 from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
 
 import bolero
 import pkgconfig
 
+# Run Cython to create cpp-sources from .pyx files
+cythonize("bolero/wrapper/_wrapper.pyx")
+cythonize("bolero/utils/_ranking_svm.pyx")
+
+# Use PkgConfig to configure the extensions
 conf = pkgconfig.parse("bl_loader bolero lib_manager")
 
-ranking = Extension('bolero/utils/_ranking_svm', ['bolero/utils/_ranking_svm.cpp'])
+
+ranking = Extension('bolero/utils/_ranking_svm', ['bolero/utils/_ranking_svm.c'])
 wrapper = Extension('bolero/wrapper/_wrapper', ['bolero/wrapper/_wrapper.cpp'],
 	include_dirs = conf["include_dirs"],
 	library_dirs = conf["library_dirs"],
 	libraries = conf["libraries"]
 )
 
+# Create the Python package with setuptools
 setup(
 	name="bolero",
 	maintainer="DFKI-RIC",
